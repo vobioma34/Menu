a854f3c6949dc5c47d4e70d6f122d3bdca71c528
Victor Obioma <vobioma34@icloud.com>
2023-07-27 13:10:50 -0500

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Victor Obioma on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
